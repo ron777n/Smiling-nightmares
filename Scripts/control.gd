97458b276a3e6a84515baa47445cc45ad2c8c035
SaltyPickleFunges <123moviesscammer@keemail.me>
2025-12-18 10:21:39 -0800

    Update

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -2,14 +2,18 @@ extends Control
 
 
 @onready var audio: AudioStreamPlayer2D = $"../AudioStreamPlayer2D"
-
+@onready var jumpscare: TextureRect = $TextureRect
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Maps/Map_1.tscn")
 
+
 func _on_quit_pressed() -> void:
+	
 	get_tree().quit()
 
 
 func _on_hit_me_pressed() -> void:
-	#audio.play()
-	pass
+	audio.play()
+	jumpscare.visible = true
+	await get_tree().create_timer(3.0).timeout
+	jumpscare.visible = false
