f160ae6b4bb80dc6574e150f46b7886d1bf53238
SaltyPickleFunges <123moviesscammer@keemail.me>
2025-12-17 18:58:37 -0800

    iitbijtbijbtijtbjitbjbt

--- Scenes/monster.gd
+++ Scenes/monster.gd
@@ -8,6 +8,5 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#look_at(target.position)
-	global_rotation = target.global_position
+func _process(_delta):
+	look_at(target.position)
