cc768a5749a003be62e8794b538094d94579c5aa
SaltyPickleFunges <123moviesscammer@keemail.me>
2025-12-18 09:29:19 -0800

    update

--- Scripts/map_1.gd
+++ Scripts/map_1.gd
@@ -1,8 +1,1 @@
 extends Node3D
-
-@onready var target: CharacterBody3D = $Player
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	get_tree().call_group("monster", "target_position", target.global_transform.origin)
--- Scripts/monster.gd
+++ Scripts/monster.gd
@@ -1,22 +1,7 @@
 extends CharacterBody3D
 
 @export var other_target: Node3D
-@onready var nav: NavigationAgent3D = $NavigationAgent3D
 
-var speed = 3400
-var _gravity = 9.8
 func _process(_delta):
 	look_at(other_target.position)
-	if not is_on_floor():
-		velocity.y -= _gravity * _delta
-	else :
-		velocity.y -= 2
-	var next_loc = nav.get_next_path_position()
-	var current_loc = global_transform.origin
-	var new_vel = (next_loc - current_loc).normalized() * speed
-	velocity = velocity.move_toward(new_vel,0.25)
-	
-	move_and_slide()
-
-func target_pos(target):
-	nav.target_position = target
+	velocity.angle_to(rotation)
