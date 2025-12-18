c02667acff4f6fa31026523611bb3583a3665d45
SaltyPickleFunges <123moviesscammer@keemail.me>
2025-12-17 19:25:37 -0800

    update 5656

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -1,9 +1,14 @@
 extends Control
 
 
+@onready var audio: AudioStreamPlayer2D = $"../AudioStreamPlayer2D"
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Maps/Map_1.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_hit_me_pressed() -> void:
+	audio.play()
